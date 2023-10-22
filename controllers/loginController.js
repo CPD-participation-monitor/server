43b4b7997b719d492d5322bc49d22d9ebf723074
thevindu-w <thevinduwijesekera99@gmail.com>
2023-10-22 15:52:25 +0530

    Add HTTP status codes for failed logins

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,30 +4,30 @@ const loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
         if (!email) {
-            res.json({ 'success': false, 'reason': 'Email cannot be empty' });
+            res.status(400).json({ 'success': false, 'reason': 'Email cannot be empty' });
             return;
         }
         if (!Validator.validate('email', email)) {
-            res.json({ 'success': false, 'reason': 'Invalid email format' });
+            res.status(400).json({ 'success': false, 'reason': 'Invalid email format' });
             return;
         }
         if (!password) {
-            res.json({ 'success': false, 'reason': 'Password cannot be empty' });
+            res.status(400).json({ 'success': false, 'reason': 'Password cannot be empty' });
             return;
         }
         if (!Validator.validate('password', password)) {
-            res.json({ 'success': false, 'reason': 'Invalid password format' });
+            res.status(400).json({ 'success': false, 'reason': 'Invalid password format' });
             return;
         }
         // TODO: Change this to check password and issue JWT
         if (email === password) {
-            res.json({ 'success': true });
+            res.status(200).json({ 'success': true });
         } else {
-            res.json({ 'success': false, 'reason': 'Incorrect password' });
+            res.status(401).json({ 'success': false, 'reason': 'Incorrect password' });
         }
     } catch (err) {
         console.log(err);
-        res.json({ 'success': false, 'reason': 'Error occured' });
+        res.status(500).json({ 'success': false, 'reason': 'Error occured' });
     }
 };
 
