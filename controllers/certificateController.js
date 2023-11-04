87e90ecd2d008261a820d471a06660728c9daa42
thevindu-w <thevinduwijesekera99@gmail.com>
2023-11-05 01:46:03 +0530

    Add method to verify certificate

--- controllers/certificateController.js
+++ controllers/certificateController.js
@@ -1,25 +1,56 @@
 const crypto = require('crypto');
 const fs = require('fs');
-const { getUserAllSessions } = require("../utils/sessionHelper");
+const { getUserSessions, getUserAllSessions } = require("../utils/sessionHelper");
 
 // See keys/README.md on how to generate this key
 const private_key = fs.readFileSync('keys/privateKey.pem', 'utf-8');
+const public_key = fs.readFileSync('keys/publicKey.pem', 'utf-8');
 
 const issueCertificate = (req, res) => {
     try {
         const email = req.email;
-        getUserAllSessions(email, (sessions) => {
+        let sessions = null;
+        if (req.body.sessions) {
+            sessions = req.body.sessions;
+        }
+        cb = sessions => {
             const certData = { email, sessions };
-            const signer = crypto.createSign('sha256');
-            signer.write(JSON.stringify(certData));
-            signer.end();
-            const signature = signer.sign(private_key, 'base64');
-            res.json({ 'data': certData, signature });
-        })
+            if (sessions.length > 0) {
+                const signer = crypto.createSign('sha256');
+                signer.write(JSON.stringify(certData));
+                signer.end();
+                const signature = signer.sign(private_key, 'base64');
+                res.json({ 'data': certData, signature });
+            } else {
+                // no data => nothing to sign
+                res.json({ 'data': certData });
+            }
+        }
+        if (sessions) {
+            getUserSessions(email, sessions, cb);
+        } else {
+            getUserAllSessions(email, cb);
+        }
     } catch (err) {
         console.log(err);
         res.status(500).json({ 'success': false, 'reason': 'Error occured' });
     }
 };
 
-module.exports = { issueCertificate };
+const verifyCertificate = (req, res) => {
+    try {
+        let { data, signature } = req.body;
+        if (!data || !signature) {
+            req.status(400).json({ 'success': false, 'reason': 'Invalid request' });
+        }
+        const verify = crypto.createVerify('sha256');
+        verify.write(JSON.stringify(data));
+        verify.end();
+        res.json({ 'valid': verify.verify(public_key, signature, 'base64') });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ 'success': false, 'reason': 'Error occured' });
+    }
+};
+
+module.exports = { issueCertificate, verifyCertificate };
--- routes/certificate.js
+++ routes/certificate.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { issueCertificate } = require('../controllers/certificateController');
+const { issueCertificate, verifyCertificate } = require('../controllers/certificateController');
 
 const router = express.Router();
 
-router.post('/certificate', issueCertificate);
+router.post('/certificate/generate', issueCertificate);
+router.post('/certificate/verify', verifyCertificate);
 
 module.exports = router;
