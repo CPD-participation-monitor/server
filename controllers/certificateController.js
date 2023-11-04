544a8c5be01240bf14e4a13900988150ead30dce
thevindu-w <thevinduwijesekera99@gmail.com>
2023-11-05 01:16:28 +0530

    Implement issuing certificates

--- controllers/certificateController.js
+++ controllers/certificateController.js
@@ -1,11 +1,20 @@
-const { addSession, addUserSession, addAllUsersSession, getUserSession, getUserAllSessions } = require("../utils/sessionHelper");
+const crypto = require('crypto');
+const fs = require('fs');
+const { getUserAllSessions } = require("../utils/sessionHelper");
+
+// See keys/README.md on how to generate this key
+const private_key = fs.readFileSync('keys/privateKey.pem', 'utf-8');
 
 const issueCertificate = (req, res) => {
     try {
         const email = req.email;
         getUserAllSessions(email, (sessions) => {
-            console.log(sessions);
-            res.json(sessions);
+            const certData = { email, sessions };
+            const signer = crypto.createSign('sha256');
+            signer.write(JSON.stringify(certData));
+            signer.end();
+            const signature = signer.sign(private_key, 'base64');
+            res.json({ 'data': certData, signature });
         })
     } catch (err) {
         console.log(err);
--- utils/sessionHelper.js
+++ utils/sessionHelper.js
@@ -16,30 +16,39 @@ const addAllUsersSession = () => {
 
 }
 
-const getUserSession = () => {
-
+const filterSessions = (email, results) => {
+    let sessions = [];
+    for (const item of results) {
+        const name = item.name;
+        const organization = item.org;
+        const date = item.date;
+        const hmac = item.hmac;
+        const hmacCreator = createHmac('sha256', KEY);
+        hmacCreator.update(`${email}|${name}|${organization}|${date}`, 'utf8');
+        let hmacExpected = hmacCreator.digest().toString('base64');
+        if (hmac !== hmacExpected) {
+            console.log('HMAC not matching');
+            continue;
+        }
+        sessions.push({name, organization, date});
+    }
+    return sessions;
 }
 
-const getUserAllSessions = (email, cb) => {
-    con.query('SELECT name, org, date, hmac FROM user_session INNER JOIN session USING (sessionId) where email = ?', [email], function (err, result) {
+const getUserSessions = (email, sessionList, cb) => {
+    con.query('SELECT name, org, date, hmac FROM user_session INNER JOIN session USING (sessionId) WHERE email = ? AND sessionId in (?)', [email, sessionList], function (err, result) {
         if (err) throw err;
-        let sessions = [];
-        for (const item of result) {
-            const name = item.name;
-            const organization = item.org;
-            const date = item.date;
-            const hmac = item.hmac;
-            const hmacCreator = createHmac('sha256', KEY);
-            hmacCreator.update(`${email}|${name}|${organization}|${date}`, 'utf8');
-            let hmacExpected = hmacCreator.digest().toString('base64');
-            if (hmac !== hmacExpected) {
-                console.log('HMAC not matching');
-                continue;
-            }
-            sessions.push({name, organization, date});
-        }
+        let sessions = filterSessions(email, result);
         cb(sessions);
     });
 }
 
-module.exports = { addSession, addUserSession, addAllUsersSession, getUserSession, getUserAllSessions };
+const getUserAllSessions = (email, cb) => {
+    con.query('SELECT name, org, date, hmac FROM user_session INNER JOIN session USING (sessionId) WHERE email = ?', [email], function (err, result) {
+        if (err) throw err;
+        let sessions = filterSessions(email, result);
+        cb(sessions);
+    });
+}
+
+module.exports = { addSession, addUserSession, addAllUsersSession, getUserSessions, getUserAllSessions };
